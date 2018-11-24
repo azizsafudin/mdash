b9a0f8f6f1cdd95a1dbacddc1f48bfdcebae81bb
xmodulus <azizsafudin@yahoo.com.sg>
2018-11-24 23:08:58 +1300

    Removed bulma extensions from default build

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -5,4 +5,5 @@
 export { default as welcome } from './welcome.vue'
 export { default as clock } from './clock.vue'
 export { default as date } from './date.vue'
+// export { default as weather } from './weather.vue'
 
